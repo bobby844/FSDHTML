aaaab870bb98178e2296c98b119fe73fccf29fc6
bobby844 <bobbyrajput2407@gmail.com>
2024-12-04 12:16:29 +0530

    intro to react

---
+++ CSSWork/js work/index.js
@@ -0,0 +1,83 @@
+// console.log("hello world");
+// var a=12;
+// if(a>10){
+//     var b=20;
+//     console.log("a inside:"+a);
+// }
+// console.log{"a outside:"+b};
+//function fun(a,b){
+  //  console.log("hill");
+//    return a+b;
+//const result=fun();
+//console.log(result);
+
+
+// console.log("Hi,inside index.js");
+// function fun(a,b){
+//     console.log("Hiiii");
+//     return a+b;
+// }
+// const result=fun(23,12);
+// console.log(result);
+
+// function cCompiler()  
+// {
+//     return "c compiler";
+// }
+// function javacompiler(){
+//     return "Java Compiler";
+// }
+// function selectlanguage(clbk){
+//     let data;
+//     if(language=='c')
+//     return "Hi,you have selected:"+clbk();
+// }
+// console.log(selectlanguage(javacompiler)); 
+
+
+
+// const button=document.getElementById("btn");
+// const output=document.getElementById("output");
+// const select=document.getElementById('Select');
+
+// function selectLanguage(language)
+// {
+//     console.log("hi inside select");
+  
+//     let data;
+//     if(language=='c'){
+//     function cCompiler()
+//     {
+//     return " C Compiler"
+//     }
+//     data=cCompiler();
+// }
+//     if(language=="java"){
+//     function javaCompiler()
+//     {
+//     return " Java Compiler"
+//     }
+//     data=javaCompiler();
+// }
+// return data;
+// }
+// button.addEventListener("click",()=>{
+//     const comp=selectLanguage('java');
+// output.innerHTML=`<h2 style=color:red>${comp};</style>`
+//   console.log(comp);   
+// });
+// ////
+// const mypromise=new Promise(
+//     (resolve,reject)=>{
+//         let a=4;
+//     if(a<10){
+//         resolve({name:"rahul",branch:'IT'});
+//     }
+//     else{
+//         reject("Data is rejected");
+//     }
+// } 
+// );
+// mypromise.then(msg=>console.log(msg))
+// .catch(error=>console.log(error));
+// .finally(()=>console.log("finally execute")); 
---
+++ React work/app.jsx
@@ -0,0 +1,10 @@
+import {Routes, Route} from "react-router-dom"
+const App=()=>{
+    return(
+        <div>
+            <Route>
+                <Route path="/" element
+            </Route>
+        </div>
+    )
+}
